fc19a3cf8e8d74f123eb87fa0bb87a1681c67b95
ShevaRicko <66115829+ShevaRicko@users.noreply.github.com>
2020-05-29 18:06:48 +0800

    Add files via upload

---
+++ footer_template.php
@@ -0,0 +1,36 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.3-pre
+    </div>
+  </footer>
+
+<!-- jQuery -->
+<script src="../assets/js/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../assets/js/jquery-ui.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../assets/js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="../assets/js/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="../assets/js/jquery.vmap.min.js"></script>
+<script src="../assets/js/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="../assets/js/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="../assets/js/moment.min.js"></script>
+<script src="../assets/js/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../assets/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="../assets/js/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../assets/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../assets/js/adminlte.js"></script>
+</body>
+</html>
---
+++ sidebar_template.php
@@ -0,0 +1,37 @@
+   <!-- Main Sidebar Container -->
+   <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+            
+              <p>
+                Users
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link active">
+                
+                  <p>Admin</p>
+                </a>
+              </li>
+
+            </ul>
+          </li>
+
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
